af9aad785f57c8fa011dbf1d7fde2c7525a1d923
Lkabeer <ahmed@Lkabeer.com>
2018-07-27 21:42:12 +0200

    version 3

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var spinnerX = document.querySelector('.spinnerX');
 var pageX = document.querySelector('#pageX');
 var inputX = document.getElementById('inputX');
+var titleX = document.getElementById('titleX');
 
 window.addEventListener('load', function () {
     spinnerX.style.display = 'none';
@@ -15,4 +16,12 @@ particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {
 
 setTimeout( function(){
     inputX.focus();
-    }, 3300);
+}, 3300);
+
+inputX.addEventListener('blur', function() {
+    if(this.value == 178) {
+        console.log('right answer');
+        titleX.innerText = "You got 10% off ツ"
+    }
+    this.value = "";
+});
